01e050ede272430daba9fbcb7c17aa46ed6a1b00
liulei <992060356@qq.com>
2021-07-14 16:19:07 +0800

    'Test2'

--- src/naicha.java
+++ src/naicha.java
@@ -4,6 +4,7 @@ public class naicha {
     public static void main(String[] args) {
         Scanner scanner =new Scanner(System.in);
         int n = scanner.nextInt();
+		//11
         for (int i = 0; i < n*2; i++) {
 
         }
